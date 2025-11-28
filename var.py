57fbb3c9fde4a724601c43a516fd0b813f0b06e5
B0X <83914919+BOLTB0X@users.noreply.github.com>
2025-11-28 17:08:21 +0900

    - ing

---
+++ var.py
@@ -0,0 +1,16 @@
+age = 30
+name = "B0X"
+is_student = True
+
+x = 10      # x는 int(정수형)
+x = "Hello" # x는 str(문자열) 로 변경됨
+y = 3.14   # y는 float(실수형)
+y = 2.718  # y는 float(실수형) 로 변경됨
+
+str_num = "123"
+int_num = int(str_num) # int_num = 123
+
+age = 55
+message = "내 나이는 " + str(age) + "살" # "내 나이는 55살"
+
+pi = int(3.14) # pi = 3 (정수형. 소수점 이하 버림)
